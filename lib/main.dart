1f595171f27581181883d112ab42ce8f1d76b4ff
mazab99 <mazab15499@gmail.com>
2023-03-24 16:12:40 +0200

    add return nothing ex

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '1 - Nothing/ui.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      useInheritedMediaQuery: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: NothingExample(),
+    );
+  }
+}
